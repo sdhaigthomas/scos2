1d32f842735b28fc5c3b2bf33b556451a4700e21
Sam Haig-Thomas <sam@samscomputer.localdomain>
2023-01-02 19:46:26 +0000

    tideyed len self.sharlist into a var in gameplay in buy and shortened loop to oneline

--- gameplay.py
+++ gameplay.py
@@ -4,7 +4,7 @@ from portfolio import Portfolio
 from time import sleep
 from os import system
 class GamePlay:
-
+###################################################################################################################################1
     def __init__(self):
         self.stilton = Shares("Stilton Plc", 152550, 153070, 2100,"Cheese Manufacturer")
         self.parmesan = Shares("Parmesan Plc", 52320, 10323, 400,"Cheese Manufacturer")
@@ -12,25 +12,25 @@ class GamePlay:
         self.blueCheese = Shares("Blue Cheese(uhgh) Plc", 10, 13, 100,"Cheese Manufacturer")
         self.shareList = [self.stilton, self.parmesan, self.cheddar, self.blueCheese]
         self.greet()
-
+###################################################################################################################################
     def fullPriceChanges(self):
         for i in self.shareList:
             i.bid = i.stockPriceChange(i.vol, i.bid)
             i.offer = i.bid
             i.offer += i.offerGenerator(i.vol)
-
+###################################################################################################################################
     def greet(self):
         print("Welcome to stocks!")
         name = input("Please Enter a Name:\n")
         self.player = Player(name, 10000)
         print("Welcome,", self.player.getName())
-
+###################################################################################################################################
     def HUD(self):
         self.clear()
         self.lines()
         for i in self.shareList: print(i.name + ": Bid:", f'${i.bid/100:.2f}',"| Offer:", f'${i.offer/100:.2f}',"| Vol:" , i.vol/100)
         self.lines()
-
+###################################################################################################################################
     def options(self):
         choice = input("Would you like to, Buy[1], Sell[2], Wait Until Tomorrow[Enter] or Statistics[3]\n")
         if choice == "1" or choice == "2" or choice == "3" or choice == "":
@@ -47,12 +47,12 @@ class GamePlay:
     def buy(self):
         counter = 0
         toBuy = None
+        lenShareList = len(self.shareList)
         self.lines()
         print("BUY")
-        for i in range(len(self.shareList)):
-            print("Name:",self.shareList[i].name,"|","Press", i + 1, "to buy.")
+        for i in range(lenShareList): print("Name:",self.shareList[i].name,"|","Press", i + 1, "to buy.")
         self.lines()
-        num = self.integerValidator(1, len(self.shareList), "What share would you like to buy?") - 1
+        num = self.integerValidator(1, lenShareList, "What share would you like to buy?") - 1
         amount = self.integerValidator(1, 214483647, "How many would you like to buy?")
 ###################################################################################################################################
     def integerValidator(self, minimum, maximum, message):
