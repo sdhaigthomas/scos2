bdc16ea2f1ef1f91d2a622b79b4401018c858be8
Sam Haig-Thomas <sam@samscomputer.localdomain>
2023-01-04 19:07:07 +0000

    added portfolio gen method in gameplay and implented it in buy and sell

--- gameplay.py
+++ gameplay.py
@@ -3,14 +3,18 @@ from shares import Shares
 from portfolio import Portfolio
 from time import sleep
 from os import system
+from datetime import datetime
+
 ###################################################################################################################################
 class GamePlay:
     def __init__(self):
-        self.stilton = Shares("Stilton Plc", 152550, 153070, 2100, 0,"Cheese Manufacturer")
+        self.clear()
+        self.stilton = Shares("Stilton Plc", 152550, 153070, 12100, 0,"Cheese Manufacturer")
         self.parmesan = Shares("Parmesan Plc", 52320, 10323, 400, 0,"Cheese Manufacturer")
         self.cheddar = Shares("Cheddar Plc", 15230, 15350, 100, 0,"Cheese Manufacturer")
         self.blueCheese = Shares("doritos Plc", 10, 13, 100, 0,"Cheese Manufacturer")
         self.shareList = [self.stilton, self.parmesan, self.cheddar, self.blueCheese]
+        self.log = []
         self.greet()
 ###################################################################################################################################
     def fullPriceChanges(self):
@@ -27,33 +31,44 @@ class GamePlay:
 ###################################################################################################################################
     def HUD(self):
         self.clear()
+        self.sharesOwned("doritos Plc")
         self.lines()
         for i in self.shareList: print(i.name + ": Bid:", f'${i.bid/100:.2f}',"| Offer:", f'${i.offer/100:.2f}',"| Vol:" , i.vol/100)
         self.lines()
     def sharesOwned(self):
         pass
+    
 ###################################################################################################################################
     def options(self):
+        portfolioProp = ["date", "name","sharePrice", "noShares", "transType"]
         choice = input("Would you like to, Buy[1], Sell[2], Wait Until Tomorrow[Enter] or Statistics[3]\n")
         if choice == "1" or choice == "2" or choice == "3" or choice == "":
             if choice == "":
                 return choice
             else:
                 choice = int(choice)
-                if choice == 1: self.buy()
-                if choice == 2: self.sell()
-                if choice == 3: pass #Stats 
+                if choice == 1: self.log.append(self.portfolioGen(self.buy()))
+                if choice == 2: self.log.append(self.portfolioGen(self.sell()))
+                if choice == 3: pass
                 else:pass
         else: print("Please enter a valid option.")
 ###################################################################################################################################
     def buy(self):
+        self.clear()
         transactions = self.transactions("Buy", "buy")
-        return transactions[0], transactions[1], transactions[2]
+        return transactions
 ###################################################################################################################################
     def sell(self):
+        self.clear()
         transactions = self.transactions("Sell", "sell")
-        return transactions[0], transactions[1], transactions[2]
-############################################################################################################
+        return transactions
+###################################################################################################################################
+    def portfolioGen(self, transactions):
+        share = transactions[0]
+        amount = transactions[1]
+        transType = transactions[2]
+        return Portfolio(datetime.now(), self.shareList[share].name,self.shareList[share].offer, amount, transType)
+###################################################################################################################################
     def transactions(self, transTypeCaps, transType):
         counter = 0
         toSell = None
@@ -62,10 +77,10 @@ class GamePlay:
         print(transTypeCaps)
         for i in range(lenShareList): print("Name:",self.shareList[i].name,"| Owned", self.shareList[i].owned,"Press", i + 1, "to " + transType + ".")
         self.lines()
-        num = self.integerValidator(1, lenShareList, "What share would you like to " + transType + "?") - 1
+        which = self.integerValidator(1, lenShareList, "What share would you like to " + transType + "?") - 1
         amount = self.integerValidator(1, 214483647, "How many would you like to " + transType + "?")
-        return num, amount, transType
-############################################################################################################
+        return which, amount, transType
+############################################################################################################      aggregator
     def integerValidator(self, minimum, maximum, message):
         while True:
             num = input(message + " | Press Enter to cancel.\n")
@@ -73,7 +88,7 @@ class GamePlay:
                 return 0
             try:
                 num = int(num)
-                if num < maximum or num < minimum:
+                if num < maximum or num == maximum or num < minimum:
                     return num
                 else:
                     print("Please enter a valid number within 1 and " + str(maximum) + ".")
@@ -82,6 +97,5 @@ class GamePlay:
 ###################################################################################################################################
     def lines(self): print("----------------------------------")
 ###################################################################################################################################
-    def clear(self): pass#system("clear")
-###################################################################################################################################
-        
+    def clear(self): system("clear")
+###################################################################################################################################     
--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,7 @@
 ###################################################################################################################################
 class Portfolio:
-    def __init__(self, date, sharePrice, noShares, transType):
-        self.portfolio = {"date" : date, "sharePrice" : sharePrice, "noShares" : noShares, "transType": transType}
+    def __init__(self, date, sharePrice, noShares, transType, name):
+        self.portfolio = {"date" : date, "name" : name, "sharePrice" : sharePrice, "noShares" : noShares, "transType": transType}
     def pAndL():
         pass # print total profit and loss
     def listPortfolio():
